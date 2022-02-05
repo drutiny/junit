eb3eb8ba93abb1d5a2dc9b4bbdad4e8c59147999
Josh Waihi <josh.waihi@acquia.com>
2022-02-06 12:11:56 +1300

    Initial commit for junit support.

---
+++ src/Format/JUnit.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drutiny\JUnit\Format;
+
+use Drutiny\Report\Format;
+use Drutiny\Report\FilesystemFormatInterface;
+use Drutiny\Report\FormatInterface;
+use Drutiny\Profile;
+use Drutiny\AssessmentInterface;
+use Llaumgui\JunitXml\JunitXmlTestSuites;
+use Llaumgui\JunitXml\JunitXmlValidation;
+use Symfony\Component\Console\Output\StreamOutput;
+
+class JUnit extends Format implements FilesystemFormatInterface {
+  protected string $name = 'junit';
+  protected string $extension = 'xml';
+  protected JunitXmlTestSuites $testSuite;
+  protected string $directory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setWriteableDirectory(string $dir):void
+  {
+    $this->directory = $dir;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getExtension():string
+  {
+    return $this->extension;
+  }
+
+  protected function configure()
+  {
+      $this->twig = $this->container->get('twig');
+      $this->testSuite = new JunitXmlTestSuites('Test results');
+  }
+
+  public function render(Profile $profile, AssessmentInterface $assessment):FormatInterface
+  {
+    $suite = $this->testSuite->addTestSuite($profile->title);
+
+    foreach ($assessment->getResults() as $response) {
+      $policy = $response->getPolicy();
+
+      $test = $suite->addTest($policy->title);
+      $test->incAssertions();
+
+      switch ($response->getType()) {
+        case 'not-applicable':
+          $test->addSkipped('Not applicable.');
+          break;
+
+        case 'error':
+          $test->addError(strtr('exception', $response->getTokens()));
+          break;
+
+        case 'failure':
+          $test->addFailure($this->twig->createTemplate($policy->failure)->render($response->getTokens()));
+          break;
+      }
+
+      $test->finish();
+
+    }
+    $suite->finish();
+    return $this;
+  }
+
+  public function write():iterable
+  {
+    $filepath = $this->directory . '/' . $this->namespace . '.' . $this->extension;
+    $stream = new StreamOutput(fopen($filepath, 'w'));
+    $junit = $this->testSuite->getXml();
+    $stream->write($junit);
+    yield $filepath;
+  }
+
+}
